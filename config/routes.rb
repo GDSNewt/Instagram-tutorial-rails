54e26b59d49c8e5c1d308f420b4e37def6295e4e
Alex Newton <alex.newton@digital.cabinet-office.gov.uk>
2021-12-16 13:19:09 +0000

    Add Search feature for users

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   controllers: {registrations: 'registrations'}
-  resources :users, only: [:show]
+  resources :users, only: [:index, :show]
 
   resources :posts, only: [:index, :show, :create, :destroy] do
     resources :photos, only: [:create]
